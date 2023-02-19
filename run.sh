de6b83435d01952328dd3a8eb47e0e970751e3b2
Phantasmic Dev <74925183+PhantasmicDev@users.noreply.github.com>
2023-02-18 23:23:51 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 DATE=$(TZ=$INPUT_TIME_ZONE date +%F)
-SUBSTITUTION="## [Unreleased]\n\n## [$INPUT_VERSION] - $DATE"
+SUBSTITUTION="## [Unreleased]\n\n\n## [$INPUT_VERSION] - $DATE"
 sed -i "s|## \[Unreleased\]|${SUBSTITUTION}|g" $INPUT_CHANGELOG_PATH
